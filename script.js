a0d3a5560871fd92bd5e099d4b60f531d21a7b36
Nicholas Ryder <nryder7@gmail.com>
2020-08-01 22:36:48 -0400

    random pw generated in console

--- script.js
+++ script.js
@@ -18,65 +18,49 @@ generateBtn.addEventListener("click", writePassword);
 WHEN I click the button to generate a password
 */
 
-var lC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uC = [];
-//push lC to uC with uppercase and consolelog
-for (var i = 0; i < lC.length; i++) {
-  uC.push(lC[i].charAt(0).toUpperCase());
-}
-console.log(uC);
-  uC = uC
-//
-var nC = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var sC = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var spec = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+var characters = [];
+var passwordCollect = [];
 
-
-/*THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-*/
-
-var characterCount = prompt("How long would you like your password to be? (8 character minimum & 128 character maximum)");
+var characterCount = parseInt(prompt("How long would you like your password to be? (8 character minimum & 128 character maximum)"));
 
 if (characterCount > 7 && characterCount < 129) {
-var lowerCase = confirm("Would you like to include lowercase letters?");
+  if(confirm("Would you like to include lowercase letters?")) {
+    characters.push(lower);
+  }
 //uppercase
-var upperCase = confirm("Would you like to include Uppercase letters?");
+  if(confirm("Would you like to include uppercase letters?")) {
+    characters.push(upper);
+  }
 //numeric
-var numericChar = confirm("Would you like to include numbers?");
+  if(confirm("Would you like to include numbers?")) {
+    characters.push(numeric);
+  }
 //special characters
-var specialChar = confirm("Would you like to include special characters?");
+  if(confirm("Would you like to include special characters?")) {
+    characters.push(spec);
+  }
 }
-else {
-  alert("Please try again. 8 character minimum and has 128 character maximum.");
-  location.reload();
+  else {
+    alert("PLEASE TRY AGAIN - Password has 8 character minimum and has 128 character maximum.");
+    location.reload();
 }
+console.log(characterCount, characters)
 
 /*WHEN I answer each prompt
 THEN my input should be validated and at least one character type should be selected
 */
 
-console.log(lowerCase, upperCase, numericChar, specialChar);
-
-
-if (lowerCase) {
-
-}
-if (upperCase) {
-
-}
-if (numericChar) {
-
-}
-if (specialChar) {
+for (var j = 0; j < characterCount; j++) {
+  var randomCharacter = Math.floor(Math.random() * characters.length);
+  var randomCharacters = Math.floor(Math.random() *characters[randomCharacter].length);
+  var passCollection = characters[randomCharacter][randomCharacters];
+  passwordCollect.push(passCollection);
 
 }
 
 //THEN a password is generated that matches the selected criteria
-
-/*
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
-*/
+console.log(randomCharacter, randomCharacters, passCollection, passwordCollect);
